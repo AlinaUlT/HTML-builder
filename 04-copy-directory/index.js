ede3271758c4d9a736d1953a168387209b586683
Ian Travkin <ian_travkin@epam.com>
2022-11-07 23:21:47 +0100

    fix: rewrite directory after copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,22 +2,27 @@ const fs = require('fs');
 const src = 'files';
 const dest = 'files-copy';
 
-fs.mkdir(__dirname +'/' +dest, {recursive: true}, (err, data)=>{
-  if (err) {
-  console.log("err" + '/' + err);
-  }
-});
-fs.readdir(__dirname + '/'+ src, {withFileTypes: true},
-  async (err, files) => {
-    console.log(files);
-  for (let file of files) {
-    if (file.isFile()) 
-      console.log(`${file.name}`);
-      fs.copyFile(`${__dirname}/${src}/${file.name}`,`${__dirname}/${dest}/${file.name}`, (err,data)=> {
-        if (err) {
-        console.log(" " + err);
-        }
-      })
-  }
-});
+async function copyDirectory() {
 
+  fs.readdir(__dirname + '/' + src, { withFileTypes: true },
+    async (err, files) => {
+      await fs.promises.rm(__dirname + '/' + dest, { recursive: true });
+      fs.mkdir(__dirname + '/' + dest, { recursive: false }, async (err, data) => {
+        if (err) {
+          console.log("the directory already exists, it will be overwritten");
+        }
+      });
+
+      console.log(files);
+      for (let file of files) {
+        if (file.isFile())
+          console.log(`${file.name}`);
+        fs.copyFile(`${__dirname}/${src}/${file.name}`, `${__dirname}/${dest}/${file.name}`, (err, data) => {
+          if (err) {
+            console.log(err);
+          }
+        })
+      }
+    });
+}
+copyDirectory()
