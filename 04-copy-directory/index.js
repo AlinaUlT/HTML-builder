af6c3f1f9add765f612938ffeb5d969d8d54dc7a
Ian Travkin <ian_travkin@epam.com>
2022-11-07 12:18:10 +0100

    feat: 04 copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs');
+const src = 'files';
+const dest = 'files-copy';
+
+fs.mkdir(__dirname +'/' +dest, {recursive: true}, (err, data)=>{
+  if (err) {
+  console.log("err" + '/' + err);
+  }
+});
+fs.readdir(__dirname + '/'+ src, {withFileTypes: true},
+  async (err, files) => {
+    console.log(files);
+  for (let file of files) {
+    if (file.isFile()) 
+      console.log(`${file.name}`);
+      fs.copyFile(`${__dirname}/${src}/${file.name}`,`${__dirname}/${dest}/${file.name}`, (err,data)=> {
+        if (err) {
+        console.log(" " + err);
+        }
+      })
+  }
+});
 
